ac84e505376dc94424ad009ed82baedaf1691a2f
Mike Gossland <mike@gossland.com>
2023-01-23 18:02:02 -0800

    Move typing_test_2 to typing_test

--- typing_test.py
+++ typing_test.py
@@ -12,8 +12,12 @@ else:
     remind_at = 100
 N = 1
 typed_key = ''
+char_list = 4*string.digits + 2*'4589_=+' + string.punctuation
+f = open("wrong_keys.txt", "r")
+char_list = char_list + 2*f.read().strip()
+f.close()
 print( 'Test of {0} keys'.format(remind_at))
-print( 4*string.digits + 2*'4589_=+' + string.punctuation)
+print( char_list )
 print( "Enter 'q' to quit")
 start_time = time.time()
 num_right=0
@@ -22,56 +26,59 @@ num_typed=0
 num_test_keys=0
 test_key=''
 last_test_key=''
-looping=True
-new_key=False
+wrong_keys=''
+quit_now=False
 
-while looping:
-    test_key = ''.join(random.choices(4*string.digits + 2*'4589_=+' + string.punctuation, k=N))
-    new_key = True
-    while True:
+random.seed()
+
+while True:
+    test_key = ''.join(random.choices(char_list, k=N))
+    num_test_keys = num_test_keys + 1
+    typed_key = '';
+    while typed_key != test_key:
         print(test_key, end=' ', flush=True)
         fd = sys.stdin.fileno()
         old_settings = termios.tcgetattr(fd)
         try:
             tty.setraw(sys.stdin.fileno())          # Raw read
             typed_key = sys.stdin.read(1)
-            if typed_key != 'q':
-                num_typed = num_typed + 1
+            num_typed = num_typed + 1
         finally:
             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        if new_key and typed_key != 'q':
-            num_test_keys = num_test_keys + 1
-            new_key = False
         print(typed_key )
         if typed_key == test_key:
             num_right = num_right + 1
+        else:
+            wrong_keys = wrong_keys + test_key
         if num_test_keys % remind_at == 0:
-            print('\n{0} typed keys ----------------'.format(num_test_keys))
-            typed_key='q'
+            print('\n{0} test keys ----------------'.format(num_test_keys))
+            quit_now = True
             break
-        if typed_key == test_key or typed_key == 'q':
+        if (typed_key == 'q') and (test_key != 'q'):
             break
 
-    looping = typed_key != 'q'
+    if quit_now:
+        break
 
-    # while typed_key != test_key and typed_key != 'q':
-    #     last_test_key = test_key
-    #     if typed_key != '\n':
-    #         print(test_key, end=' ', flush=True)
-    #         num_wrong = num_wrong + 1
- 
-    #     if typed_key == test_key:
-    #         num_right = num_right + 1
+    if (typed_key == 'q') and (test_key != 'q'):
+        num_typed = num_typed - 1
+        break
 
- 
 end_time = time.time()
 duration = end_time - start_time
 num_wrong = num_typed - num_right
 
+
+print('{0} test keys, {1} typed keys, {2} right keys, {3} wrong keys'.format(num_test_keys, num_typed, num_right, num_wrong))
+print('Wrong keys were {0}'.format(wrong_keys))
+f = open("wrong_keys.txt", "w")
+f.write(wrong_keys + "\n")
+f.close()
+
 output=''
 result=''
 
-if num_test_keys > 0:
+if quit_now:
     result='{0}\tkeys\t{1:0.1f}\tsecs\t{2:0.0f}\t%\t{3:.02f}\tkps'.format(num_test_keys, duration, 100*num_right/(num_wrong + num_right), num_right/duration)
     timestamp=datetime.datetime.now()
     output = '{0}\t{1}'.format(timestamp.strftime('%Y-%m-%d %H:%M:%S'), result)
@@ -80,5 +87,5 @@ if num_test_keys > 0:
     f.write(output + "\n")
     f.close()
 else:
-    print('No keys tested')
+    print('No results saved')
 
